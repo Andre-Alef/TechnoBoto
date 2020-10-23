47adbd94832e3dc878b5b80a5efcee5478f36db5
Andre Alef Degaspari Moura <alef.moura@verithus.com.br>
2020-10-22 21:34:47 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -22,50 +22,6 @@ const client = new tmi.Client(options)
 
 client.connect()
 
-// events
-client.on('disconnected', (reason) => {
-  onDisconnectedHandler(reason)
-})
-
-client.on('connected', (address, port) => {
-  onConnectedHandler(address, port)
-})
-
-client.on('hosted', (channel, username, viewers, autohost) => {
-  onHostedHandler(channel, username, viewers, autohost)
-})
-
-client.on('subscription', (channel, username, method, message, userstate) => {
-  onSubscriptionHandler(channel, username, method, message, userstate)
-})
-
-client.on('raided', (channel, username, viewers) => {
-  onRaidedHandler(channel, username, viewers)
-})
-
-client.on('cheer', (channel, userstate, message) => {
-  onCheerHandler(channel, userstate, message)
-})
-
-client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
-  onGiftPaidUpgradeHandler(channel, username, sender, userstate)
-})
-
-client.on('hosting', (channel, target, viewers) => {
-  onHostingHandler(channel, target, viewers)
-})
-
-client.on('reconnect', () => {
-  reconnectHandler()
-})
-
-client.on('resub', (channel, username, months, message, userstate, methods) => {
-  resubHandler(channel, username, months, message, userstate, methods)
-})
-
-client.on('subgift', (channel, username, streakMonths, recipient, methods, userstate) => {
-  subGiftHandler(channel, username, streakMonths, recipient, methods, userstate)
-})
 
 // event handlers
 
@@ -89,70 +45,6 @@ client.on('message', (channel, userstate, message, self) => {
 
 function onMessageHandler (channel, userstate, message, self) {
   checkTwitchChat(userstate, message, channel)
-}
-
-function onDisconnectedHandler(reason) {
-  console.log(`Disconnected: ${reason}`)
-}
-
-function onConnectedHandler(address, port) {
-  console.log(`Connected: ${address}:${port}`)
-}
-
-function onHostedHandler (channel, username, viewers, autohost) {
-  client.say(channel,
-    `Thank you @${username} for the host of ${viewers}!`
-  )
-}
-
-function onRaidedHandler(channel, username, viewers) {
-  client.say(channel,
-    `Thank you @${username} for the raid of ${viewers}!`
-  )
-}
-
-function onSubscriptionHandler(channel, username, method, message, userstate) {
-  client.say(channel,
-    `Thank you @${username} for subscribing!`
-  )
-}
-
-function onCheerHandler(channel, userstate, message)  {
-  client.say(channel,
-    `Thank you @${userstate.username} for the ${userstate.bits} bits!`
-  )
-}
-
-function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
-  client.say(channel,
-    `Thank you @${username} for continuing your gifted sub!`
-  )
-}
-
-function onHostingHandler(channel, target, viewers) {
-  client.say(channel,
-    `We are now hosting ${target} with ${viewers} viewers!`
-  )
-}
-
-function reconnectHandler () {
-  console.log('Reconnecting...')
-}
-
-function resubHandler(channel, username, months, message, userstate, methods) {
-  const cumulativeMonths = userstate['msg-param-cumulative-months']
-  client.say(channel,
-    `Thank you @${username} for the ${cumulativeMonths} sub!`
-  )
-}
-
-function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
-
-  client.say(channel,
-    `Thank you @${username} for gifting a sub to ${recipient}}.`
-  )
-
- 
 }
 
 // commands
